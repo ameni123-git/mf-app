5d47de9c9fc697fe33425a7e61c4e8281f32baa1
ameni123-git <ameni.bahlous1@gmail.com>
2021-10-25 19:30:53 +0100

    First part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Social from "./components/social/Social";
+import Button from "./components/button/Button";
+import illustration from "./assets/undraw_Online_learning_re_qw08.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="firstpart">
+        <div className="container">
+          {/* menu et logo  */}
+          <Header />
+          <div className="firstpart__content">
+            {/* social media icons  */}
+            <Social />
+            <div>
+              
+              <h1>learn without limits</h1>
+              <p>
+                Maecenas sed dolor lorem. Maecenas quis congue turpis. Proin a
+                ipsum dictum.
+              </p>
+              <Button text="start your training" />
+            </div>
+            <img src={illustration} alt="ullustration" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
